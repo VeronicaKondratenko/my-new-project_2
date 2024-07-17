914d9e226b52a0e95af588d340cfed51a2c3be03
VeronicaLevina <ipz22-v.levina@nubip.edu.ua>
2024-07-17 12:26:00 +0300

    add a new files123

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from termcolor import colored
-
 def add(a, b):
     return a + b
 
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError(colored("Division by zero is not allowed.", "red"))
-    return a / b
-
 if __name__ == "__main__":
-    print(f"Addition result: {add(4, 2)}")
-    print(f"Subtraction result: {subtract(4, 2)}")
-    print(f"Multiplication result: {multiply(4, 2)}")
-    print(f"Division result: {divide(4, 2)}")
+    result = add(1, 2)
+    print(f"The result is {result}")
+
